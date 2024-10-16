e05a8d40e2f1126e7e450124ddba90dcdb27d808
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2024-10-16 10:29:57 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,14 +1,11 @@
 document.getElementById('next').onclick = function(){
-  //developed by kavya trivedi
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').appendChild(lists[0]);
 }
-//developed by kavya trivedi 
+
 document.getElementById('prev').onclick = function(){
-  //developed by kavya trivedi 
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
-  //developed by kavya trivedi 
 }
   
 // Function to play the sound
